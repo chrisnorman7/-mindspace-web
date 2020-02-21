c76c4d9df5a971f997fd1662b1414776a4fb89d5
Chris Norman <chris.norman2@googlemail.com>
2020-02-21 18:57:38 +0000

    Add dummy print command.

--- example.py
+++ example.py
@@ -4,6 +4,11 @@ f = MindspaceFactory()
 
 
 @f.parser.command
+def do_print(con, text):
+    print(f'{con.transport.getPeer()} says: {text}')
+
+
+@f.parser.command
 def echo(con, string):
     con.send_command('alert', string)
 
